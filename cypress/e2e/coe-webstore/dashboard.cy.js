17959a479fa51e5dc3b76b7d5f0573ffbdeabd2d
Daniels <k.daniels.andersons@gmail.com>
2024-07-31 12:00:35 +0300

    Added dashboard info check test case

--- cypress/e2e/coe-webstore/dashboard.cy.js
+++ cypress/e2e/coe-webstore/dashboard.cy.js
@@ -4,10 +4,35 @@ import Global from "../pageElements/Global";
 describe("COE Webstore dashboard functionality", () => {
   beforeEach(() => {
     Login.loginUser();
+    cy.visit("/");
   });
   it("correct user information is dispayed in the dashboard", () => {
     Global.openDashboard();
-
-    cy.get(':nth-child(1) > [data-testid="profile-link"]').click();
+    cy.fixture("defaultUserProfileData").then((userData) => {
+      //TODO this could be written better
+      cy.get(
+        `[data-testid="welcome-message"][data-value="${userData.firstName}"]`,
+      ).should("be.visible");
+      cy.getByTestId("customer-email").should(
+        "contain",
+        Cypress.env("userEmail"),
+      );
+      cy.getByTestId("profile-link")
+        .should("be.visible")
+        .first()
+        .click({ force: true });
+      cy.getByTestId("current-info")
+        .should("contain", userData.firstName)
+        .should("be.visible");
+      cy.getByTestId("current-info")
+        .should("contain", userData.lastName)
+        .should("be.visible");
+      cy.getByTestId("current-info")
+        .should("contain", Cypress.env("userEmail"))
+        .should("be.visible");
+      cy.getByTestId("current-info")
+        .should("contain", userData.phone)
+        .should("be.visible");
+    });
   });
 });
